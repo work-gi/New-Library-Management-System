6a3bbd7e67f6965d6c3d79630c155c0112e79a5b
work-gi <workzgit@gmail.com>
2024-07-30 18:10:55 +0100

    View Commit

---
+++ src/main/java/edu/ijse/lms/Main.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package edu.ijse.lms;
+
+import edu.ijse.lms.view.LoginView;
+
+/**
+ *
+ * @author ACER
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        
+         new LoginView().setVisible(true);
+    }
+}
